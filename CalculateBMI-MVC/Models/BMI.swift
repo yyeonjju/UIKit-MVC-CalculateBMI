00bca1f37334fcc2e8242e5ba280654c3bd0e45c
yyeonjju <llilyha123@gmail.com>
2023-02-22 02:29:03 +0900

    Refactor : BMI 관련된 속성으로 이루어진 구조체를 정의하고 필요시 그 구조체 인스턴스의 속성을 사용할 수 있도록 수정

---
+++ CalculateBMI-MVC/Models/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  CalculateBMI-MVC
+//
+//  Created by 하연주 on 2023/02/22.
+//
+
+import Foundation
+import UIKit
+
+
+struct BMI {
+    var value : Double
+    var matchColor : UIColor
+    var advice : String
+}
--- CalculateBMI-MVC/Models/BMICalculatorManager.swift
+++ CalculateBMI-MVC/Models/BMICalculatorManager.swift
@@ -9,52 +9,46 @@ import Foundation
 import UIKit
 
 struct BMICalculatorManager {
-    var bmi : Double?
+    var bmi : BMI = BMI(value: 0.0, matchColor: .clear , advice: "")
     
     // 구조체에서 본인의 속성을 바꾸려면 mutating 키워드 필요
     mutating func calculateBMI (h:String, w:String) {
         guard let w = Double(w) ,let h = Double(h) else{return }
-        let bmi = w / (h*h) * 10000
+        let bmiNum = w / (h*h) * 10000
         //소수점 한자리까지 반올림
-        self.bmi = round(bmi * 10)/10
-    }
-    
-    // 색깔 얻는 메서드
-    func getBackgroundColor() -> UIColor {
-        guard let bmi = bmi else { return UIColor.black }
-        switch bmi {
+        self.bmi.value = round(bmiNum * 10)/10
+        
+        switch bmi.value {
         case ..<18.6:
-            return UIColor(displayP3Red: 22/255, green: 231/255, blue: 207/255, alpha: 1)
+            bmi.matchColor = UIColor(displayP3Red: 22/255, green: 231/255, blue: 207/255, alpha: 1)
         case 18.6..<23.0:
-            return UIColor(displayP3Red: 212/255, green: 251/255, blue: 121/255, alpha: 1)
+            bmi.matchColor = UIColor(displayP3Red: 212/255, green: 251/255, blue: 121/255, alpha: 1)
         case 23.0..<25.0:
-            return UIColor(displayP3Red: 218/255, green: 127/255, blue: 163/255, alpha: 1)
+            bmi.matchColor = UIColor(displayP3Red: 218/255, green: 127/255, blue: 163/255, alpha: 1)
         case 25.0..<30.0:
-            return UIColor(displayP3Red: 255/255, green: 150/255, blue: 141/255, alpha: 1)
+            bmi.matchColor = UIColor(displayP3Red: 255/255, green: 150/255, blue: 141/255, alpha: 1)
         case 30.0...:
-            return UIColor(displayP3Red: 255/255, green: 100/255, blue: 78/255, alpha: 1)
+            bmi.matchColor = UIColor(displayP3Red: 255/255, green: 100/255, blue: 78/255, alpha: 1)
         default:
-            return UIColor.black
+            bmi.matchColor = UIColor.black
         }
-    }
-    
-    // 문자열 얻는 메서드
-    func getBMIAdviceString() -> String {
-        guard let bmi = bmi else { return "" }
-        switch bmi {
+        
+        switch bmi.value {
         case ..<18.6:
-            return "저체중"
+            bmi.advice = "저체중"
         case 18.6..<23.0:
-            return "표준"
+            bmi.advice = "표준"
         case 23.0..<25.0:
-            return "과체중"
+            bmi.advice = "과체중"
         case 25.0..<30.0:
-            return "중도비만"
+            bmi.advice = "중도비만"
         case 30.0...:
-            return "고도비만"
+            bmi.advice = "고도비만"
         default:
-            return ""
+            bmi.advice = ""
         }
+        
+        
     }
     
     
